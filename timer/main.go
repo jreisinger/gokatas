0041e4f7e0295d50327c30dd1f4e614c2abb1009
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-04-23 18:23:39 +0200

    add timer kata

---
+++ timer/main.go
@@ -0,0 +1,35 @@
+// Timer times out a goroutine that emits Go pointer types over a channel.
+// Based on "Go Programming Basics" video from John Graham-Cumming.
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan string)
+	go pointerTypes(ch)
+	for pt := range ch {
+		fmt.Println(pt)
+		time.Sleep(time.Second)
+	}
+}
+
+func pointerTypes(ch chan string) {
+	defer close(ch)
+	ptypes := []string{"slice", "map", "function", "channel", "interface"}
+	var i int
+	t := time.NewTimer(time.Second * 5)
+	for {
+		select {
+		case ch <- ptypes[i]:
+			i++
+			if i == len(ptypes) {
+				i = 0
+			}
+		case <-t.C:
+			return
+		}
+	}
+}
