443760e749aa2bbecf16f21c27ebbecc7de77399
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-04-23 17:20:42 +0200

    add pingpong kata

---
+++ pingpong/main.go
@@ -0,0 +1,37 @@
+// Pingpong shows how (un-buffered) channels are used for communication and
+// synchronization between goroutines. When pinger or ponger attempts to send a
+// message on the channel, it will wait (block) until printer is ready to
+// receive the message. Note that "ping" and "pong" are alternating.
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan string)
+	go pinger(ch)
+	go ponger(ch)
+	go printer(ch)
+
+	var enter string
+	fmt.Scanln(&enter)
+}
+
+func pinger(ch chan<- string) {
+	for {
+		ch <- "ping"
+	}
+}
+func ponger(ch chan<- string) {
+	for {
+		ch <- "pong"
+	}
+}
+func printer(ch <-chan string) {
+	for {
+		fmt.Printf("%s ", <-ch)
+		time.Sleep(time.Second)
+	}
+}
