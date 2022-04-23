a1bfd84de0b66b6b999fc7e62a868817fb2cd4d2
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-04-23 18:14:05 +0200

    add shop3 kata

---
+++ shop3/main.go
@@ -0,0 +1,43 @@
+// Shop3 is a web shop selling shoes and socks. It registers the /list and
+// /price endpoints by calling the convenience function http.HandleFunc and
+// thus using the DefaultServeMux. Adapted from
+// github.com/adonovan/gopl.io/tree/master/ch7/http3.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+type dollars float32
+
+func (d dollars) String() string {
+	return fmt.Sprintf("$%.2f", d)
+}
+
+type database map[string]dollars
+
+func (db database) list(w http.ResponseWriter, r *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, r *http.Request) {
+	item := r.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q", item)
+		return
+	}
+	fmt.Fprintf(w, "%s", price)
+}
