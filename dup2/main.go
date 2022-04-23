8fb82751def1ea7a7e8e90dca4ac2b00c4471bbf
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-04-23 14:52:30 +0200

    add dup2 and dup3 katas

---
+++ dup2/main.go
@@ -0,0 +1,40 @@
+// Dup2 prints the count and text of lines that appear more than once in the
+// input. It reads from stdin or from a list of named files. Adapted from
+// github.com/adonovan/gopl.io/tree/master/ch1/dup2.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	if len(os.Args[1:]) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range os.Args[1:] {
+			file, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(file, counts)
+			file.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(file *os.File, counts map[string]int) {
+	input := bufio.NewScanner(file)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
---
+++ dup3/main.go
@@ -0,0 +1,32 @@
+// Dup3 prints the count and text of lines that appear more than once in the
+// named input files. It reads in entire file at once ("slurp" mode). Adapted
+// from github.com/adonovan/gopl.io/tree/master/ch1/dup3.
+//
+// os.ReadFile (dup3) and bufio.Scanner (dup2) use the Read and Write methods of
+// *os.File under covers.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, arg := range os.Args[1:] {
+		b, err := os.ReadFile(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(b), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
