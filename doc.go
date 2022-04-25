c9799106a2eb18a6ae7eebbe0fd5fd89ec97a001
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-04-25 09:04:45 +0200

    minor changes in doc.go

--- doc.go
+++ doc.go
@@ -2,9 +2,10 @@
 Package gokatas contains programming katas for memorizing and perfecting your Go
 movements. Katas are simple, practical and well written Go programs.
 
-    go run cmd/katas.go  # what kata should I practice => e.g. areader
+    go run cmd/katas.go  # what kata should I practice today => e.g. areader
     go doc -all areader  # what's the idea and design of the kata
     code areader         # let's practice: delete and try to write it back
-    go run areader/cmd/* # check it's working; or `go test dac/*`
+    go run areader/cmd/* # check it's working; or `go test dac/*` or `git diff`
+    code -a katas.md     # write down katas you've done
 */
 package gokatas
