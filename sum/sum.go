d426a95bbf923b3cc219594e7080c7da860ff146
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-05-02 08:17:20 +0200

    rewrite dac into sum

---
+++ sum/sum.go
@@ -0,0 +1,18 @@
+// Package sum sums a list of integers using loop and divide-and-conquer
+// technique. See reisinge.net/notes/cs/divide-and-conquer for more.
+package sum
+
+func Loop(list []int) int {
+	var sum int
+	for _, n := range list {
+		sum += n
+	}
+	return sum
+}
+
+func DaC(list []int) int {
+	if len(list) == 0 {
+		return 0
+	}
+	return list[0] + DaC(list[1:])
+}
---
+++ sum/sum_test.go
@@ -0,0 +1,28 @@
+package sum
+
+import "testing"
+
+var tests = []struct {
+	input []int
+	want  int
+}{
+	{[]int{}, 0},
+	{[]int{0}, 0},
+	{[]int{1, 3, 2}, 6},
+}
+
+func TestDaC(t *testing.T) {
+	for _, test := range tests {
+		if got := DaC(test.input); got != test.want {
+			t.Errorf("DaC(%v) = %d, want %d", test.input, got, test.want)
+		}
+	}
+}
+
+func TestLoop(t *testing.T) {
+	for _, test := range tests {
+		if got := Loop(test.input); got != test.want {
+			t.Errorf("Loop(%v) = %d, want %d", test.input, got, test.want)
+		}
+	}
+}
