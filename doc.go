902416eb8275d23373b77491f10be50d97e59738
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-05-03 09:11:10 +0200

    it's important to create a habit

--- doc.go
+++ doc.go
@@ -5,7 +5,11 @@ movements. Katas are simple, practical and well written Go programs.
     go run cmd/katas.go  # what kata should I practice today => e.g. areader
     go doc -all areader  # what's the idea and design of the kata
     code areader         # let's practice: delete and try to write it back
-    go run areader/cmd/* # check it's working; or `go test dac/*` or `git diff`
+    go run areader/cmd/* # check it's working; or `git diff`
     code -a katas.md     # write down katas you've done
+
+It's important to practice regularly, to create a habit. Start by taking baby
+steps, e.g. 15 minutes a day. After some time it will require much less will
+power, it will become natural for you. 🥋
 */
 package gokatas
