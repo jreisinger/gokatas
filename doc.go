29b7d912072e6fa32f4a0351261489245cdd1f3a
PJadwin97 <patrick.jadwin@cbsinteractive.com>
2022-05-04 14:21:50 -0400

    reflect the markdown format in the go documentation so that it is clear to the user of this application

--- doc.go
+++ doc.go
@@ -7,5 +7,11 @@ movements. Katas are simple, practical and well written Go programs.
     code areader         # let's practice: delete and try to write it back
     go run areader/cmd/* # check it's working; or `git diff`
     code -a katas.md     # write down katas you've done
+
+katas.md format:
+* yyyy-mm-dd: areader
+
+katas.md format for multiple katas completed in one day:
+* yyyy-mm-dd: clock2, areader, netcat1
 */
 package gokatas
