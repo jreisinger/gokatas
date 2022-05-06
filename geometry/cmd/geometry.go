cac858b8d02c09ed99a0c8cf4d436370a72138a0
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-05-06 20:35:52 +0200

    add geometry kata

---
+++ geometry/cmd/geometry.go
@@ -0,0 +1,18 @@
+// Geometry calculates distance between two points and the perimeter of a right
+// triangle.
+package main
+
+import (
+	"fmt"
+
+	"github.com/jreisinger/gokatas/geometry"
+)
+
+func main() {
+	p := geometry.Point{1, 1}
+	q := geometry.Point{5, 4}
+	fmt.Println(p.Distance(q))
+
+	perim := geometry.Path{p, {5, 1}, q, {1, 1}}
+	fmt.Println(perim.Distance())
+}
