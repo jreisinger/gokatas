271364ba93eae937f040e798cae415ec3cccda27
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-05-09 16:28:06 +0200

    add tictactoe kata

---
+++ tictactoe/main.go
@@ -0,0 +1,32 @@
+// Tictactoe creates a a tic-tac-toe board as a slice of string slices. Adapted
+// from tour.golang.org/moretypes/14.
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	board := [][]string{
+		// []string{" ", " ", " "},
+		{" ", " ", " "},
+		{" ", " ", " "},
+		{" ", " ", " "},
+	}
+
+	// The players take turns and play like this:
+	// x   o
+	//   x
+	// o   x
+	board[0][0] = "x"
+	board[0][2] = "o"
+	board[1][1] = "x"
+	board[2][0] = "o"
+	board[2][2] = "x"
+
+	// for i := 0; i < len(board); i++ {
+	for i := range board {
+		fmt.Println(strings.Join(board[i], " "))
+	}
+}
