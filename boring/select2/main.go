590cf65b3b4a417e31154d613e2ece0c391c23c0
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-08-03 08:05:32 +0200

    add boring/select2 kata

---
+++ boring/select2/main.go
@@ -0,0 +1,41 @@
+// Use quit channel to stop the conversation. Also wait for them to tell us
+// they're done talking.
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	quit := make(chan string)
+	c := boring("blah", quit)
+	for i := rand.Intn(10); i >= 0; i-- {
+		fmt.Println(<-c)
+	}
+	quit <- "bye"
+	fmt.Printf("they say: %q\n", <-quit)
+}
+
+func boring(msg string, quit chan string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			n := rand.Intn(1e3)
+			time.Sleep(time.Duration(n) * time.Millisecond)
+
+			select {
+			case c <- fmt.Sprintf("%s %d", msg, i):
+				// do nothing
+			case <-quit:
+				cleanup()
+				quit <- "see you"
+				return
+			}
+		}
+	}()
+	return c
+}
+
+func cleanup() {}
