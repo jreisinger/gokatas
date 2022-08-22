455f3fbdf40dd97069c264ae73fd4f67397b3fe0
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-08-22 10:02:52 +0200

    minor changes in doc.go

--- boring/doc.go
+++ boring/doc.go
@@ -1,7 +1,7 @@
 // Package boring holds various Go concurrency patterns.
 //
 // Based on Go Concurrency Patterns by Rob Pibe (2012):
-// 	Slides:	https://talks.golang.org/2012/concurrency.slide
-// 	Code:	https://talks.golang.org/2012/concurrency/support
-// 	Video:	https://www.youtube.com/watch?v=f6kdp27TYZs
+// 	Slides	https://talks.golang.org/2012/concurrency.slide
+// 	Code	https://talks.golang.org/2012/concurrency/support
+// 	Video	https://www.youtube.com/watch?v=f6kdp27TYZs
 package boring
