8983695305098992539506cdd20dcd38807be54a
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-09-10 23:18:42 +0200

    minor changes in areader.go, main.go

--- autoescape/main.go
+++ autoescape/main.go
@@ -1,6 +1,9 @@
 // Autoescape shows how to suppress html/template auto-escaping behavior for
 // fields that contain trusted HTML data. (The same can be done for trusted
 // JavaScript, CSS and URLs.) Taken from gopl.io ch4.6 Text and HTML templates.
+//
+// Level: beginner
+// Topics: html/template
 package main
 
 import (
