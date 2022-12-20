bb29ecb8615fa4110786483c3cbc489701a46414
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-12-20 07:56:42 +0100

    minor changes

--- z/lookup-v0.sh
+++ z/lookup-v0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # The task is to find out which domains are on Cloudflare nameservers. This is
-# the first thought how to do it.
+# the first thought.
 
-head testdata/domains.txt | xargs -I{} dig NS {} | grep -E 'IN\s+NS.*\.$'
+head testdata/domains.txt | xargs -I {} dig NS {} | grep -E 'IN\s+NS.*\.$'
