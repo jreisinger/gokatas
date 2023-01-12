586a0ef9975c7d9a042ca5565f76af1c649f2bb7
Jozef Reisinger <jozef.reisinger@gmail.com>
2023-01-12 08:35:07 +0100

    add echo kata showing command testing

---
+++ echo/echo.go
@@ -0,0 +1,38 @@
+// Echo prints its command-line arguments.
+//
+// Adapted from github.com/adonovan/gopl.io/blob/master/ch11/echo
+//
+// Level: beginner
+// Topics: command, testing
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+var (
+	n = flag.Bool("n", false, "omit traling newline")
+	s = flag.String("s", " ", "separator")
+)
+
+var out io.Writer = os.Stdout // modified during testing
+
+func main() {
+	flag.Parse()
+	if err := echo(!*n, *s, flag.Args()); err != nil {
+		fmt.Fprintf(os.Stderr, "echo: %v\n", err)
+		os.Exit(1) // always call os.Exit or log.Fatal only from main
+	}
+}
+
+func echo(newline bool, sep string, args []string) error {
+	fmt.Fprint(out, strings.Join(args, sep))
+	if newline {
+		fmt.Fprintln(out)
+	}
+	return nil
+}
