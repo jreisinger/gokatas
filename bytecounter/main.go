5117fc00fbc0f15e31f4c5ec86d737090147dcae
Jozef Reisinger <jozef.reisinger@gmail.com>
2023-01-31 08:55:22 +0100

    minor changes

--- bytecounter/main.go
+++ bytecounter/main.go
@@ -1,6 +1,7 @@
 // Bytecounter shows how to use interfaces. It implements a concrete type named
-// ByteCounter whose Write method counts bytes. Since ByteCounter satisfies the
-// io.Writer interface (an abstract type), we can pass it to fmt.Fprint.
+// ByteCounter whose Write method counts bytes before discarding them. Since
+// ByteCounter satisfies the io.Writer interface (an abstract type), we can pass
+// it to fmt.Fprint.
 //
 // Adapted from github.com/adonovan/gopl.io/blob/master/ch7/bytecounter.
 //
@@ -13,7 +14,7 @@ import "fmt"
 type ByteCounter int
 
 func (c *ByteCounter) Write(p []byte) (int, error) {
-	*c += ByteCounter(len(p)) // explicitly convert int to ByteCounter
+	*c += ByteCounter(len(p)) // must explicitly convert int to ByteCounter
 	return len(p), nil
 }
 
