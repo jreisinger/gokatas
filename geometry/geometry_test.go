eb14f996b2f8178184070c169a86872518058e13
Jozef Reisinger <jozef.reisinger@gmail.com>
2023-02-08 08:40:08 +0100

    follow docs naming convetion: pkg.go.dev/testing#hdr-Examples

--- geometry/geometry_test.go
+++ geometry/geometry_test.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 )
 
-func ExamplePoint() {
+func ExamplePoint_Distance() {
 	p := Point{1, 1}
 	q := Point{5, 4}
 	fmt.Println(p.Distance(q))
 	// Output: 5
 }
 
-func ExamplePath() {
+func ExamplePath_Distance() {
 	perim := Path{{1, 1}, {5, 1}, {5, 4}, {1, 1}}
 	fmt.Println(perim.Distance())
 	// Output: 12
