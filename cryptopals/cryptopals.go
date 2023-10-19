01e1d6326620aa7951a4a1f17544061582d36fed
Jozef Reisinger <jozef.reisinger@gmail.com>
2023-10-19 17:19:36 +0200

    daily kata

--- cryptopals/cryptopals.go
+++ cryptopals/cryptopals.go
@@ -1,11 +1,9 @@
-// Cryptopals shows basic crypto operations.
-//
-// Always operate on raw bytes, never on encoded strings. Only use hex and
-// base64 for pretty-printing.
+// Cryptopals shows basic crypto operations. Always operate on raw bytes, never
+// on encoded strings. Only use hex and base64 for pretty-printing.
 //
 // Adapted from https://github.com/0xfe/cryptopals.
 //
-// Level: intermediate
+// Level: beginner
 // Topics: encoding, xor
 package cryptopals
 
@@ -14,31 +12,29 @@ import (
 	"encoding/hex"
 )
 
-// HexToBase64 decodes hex string h into bytes and then bytes into base64
-// string. (Set 1, challenge 1.)
+// HexToBase64 decodes hex string into base64 string. (Set 1, challenge 1.)
 func HexToBase64(h string) (string, error) {
-	data, err := hex.DecodeString(h)
+	b, err := hex.DecodeString(h)
 	if err != nil {
 		return "", err
 	}
-	return base64.RawStdEncoding.EncodeToString(data), nil
+	return base64.RawStdEncoding.EncodeToString(b), nil
 }
 
-// FixedXOR gets the bytes represented by h1 and h2 hex strings. Then it does
-// XOR bitwise operation on these two byte slices, byte by byte. H1 and h2 must
-// be of equal length. (Set 1, challenge 2.)
+// FixedXOR does bitwise XOR of two hex strings. The strings must be of equal
+// length. (Set 1, challenge 2.)
 func FixedXOR(h1, h2 string) (string, error) {
-	data1, err := hex.DecodeString(h1)
+	b1, err := hex.DecodeString(h1)
 	if err != nil {
 		return "", err
 	}
-	data2, err := hex.DecodeString(h2)
+	b2, err := hex.DecodeString(h2)
 	if err != nil {
 		return "", err
 	}
-	out := make([]byte, len(data1))
-	for i := range data1 {
-		out[i] = data1[i] ^ data2[i]
+	out := make([]byte, len(b1))
+	for i := range b1 {
+		out[i] = b1[i] ^ b2[i]
 	}
 	return hex.EncodeToString(out), nil
 }
