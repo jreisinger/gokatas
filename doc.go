d858e5c7aea8ac33bd95e2040f6bf06cca302e61
Jozef Reisinger <jozef.reisinger@gmail.com>
2023-12-18 10:16:13 +0100

    update doc

--- doc.go
+++ doc.go
@@ -4,8 +4,8 @@ Go movements. Katas are brief, well written Go programs.
 
 Here's a typical practice cycle:
 
-	go run cmd/gokatas.go # what kata should I practice today; e.g. areader
-	code areader          # understand, delete and try to write the code back
-	vi katas.md           # write down katas you've done (go doc KatasFile)
+	go run cmd/gokatas.go -c 2  # what kata should I practice today
+	code .                      # understand, delete and try to write the code back
+	vi katas.md                 # write down katas you've done (go doc KatasFile)
 */
 package gokatas
