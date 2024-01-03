7553d4c49605467e59b6961f78a592a5910b973f
Jozef Reisinger <jozef.reisinger@gmail.com>
2024-01-03 18:35:53 +0100

    daily kata

--- counter/counter.go
+++ counter/counter.go
@@ -3,9 +3,9 @@
 // Encapsulation (information hiding) is a key aspect of OOP. It prevents clients
 // from accessing variables or methods of an object. Benefits:
 //
-//  1. Clients need to understand fewer statements.
+//  1. Clients need to inspect fewer statements.
 //  2. Clients don't depend on things that might change.
-//  3. Clients can't mess with variables directly.
+//  3. Clients can't set object's variables arbitrarily.
 //
 // Adapted from the gopl.io ch. 6.6 Encapsulation.
 //
@@ -13,7 +13,7 @@
 // Topics: oop, encapsulation
 package counter
 
-type Counter struct{ n int } // counter encapsulates (hides) its field
+type Counter struct{ n int } // Counter encapsulates (hides) its field
 
 func (c *Counter) N() int     { return c.n } // getter
 func (c *Counter) Increment() { c.n++ }      // setter
